7f7096b967bff44e35d1f92b2ebebd28e254cfa7
AnthonyCorrado <amcstudios@yahoo.com>
2015-09-26 20:18:28 -0700

    basic leap motion and unity demo setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundry
+{
+	public float xMin, xMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundry boundry;
+
+	void Update ()
+	{
+		Instantiate (object, position, rotation);
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		// float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3 
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundry.xMin, boundry.xMax),
+				0.0f,
+				0.0f
+			);
+	}
+}
